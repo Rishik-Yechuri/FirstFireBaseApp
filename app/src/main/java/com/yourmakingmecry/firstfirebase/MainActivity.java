7fc8d7bab8470f390f3d2c9ff4d8ea3cb9788026
Prathad <56663764+Prathad@users.noreply.github.com>
2020-06-07 15:03:51 -0500

    Another
    Function Called

--- app/src/main/java/com/yourmakingmecry/firstfirebase/MainActivity.java
+++ app/src/main/java/com/yourmakingmecry/firstfirebase/MainActivity.java
@@ -3,12 +3,21 @@ package com.yourmakingmecry.firstfirebase;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.google.firebase.functions.FirebaseFunctions;
 
 public class MainActivity extends AppCompatActivity {
+    TextView statusText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        statusText = findViewById(R.id.statusText);
+        FirebaseFunctions.getInstance() // Optional region: .getInstance("europe-west1")
+                .getHttpsCallable("createGame")
+                .call();
     }
 }
