7a73ef43ceb452a0aef0c8bd3d6e99ee6b20c84c
Prathad <56663764+Prathad@users.noreply.github.com>
2020-06-07 00:03:39 -0500

    BP
    please

---
+++ app/src/main/java/com/yourmakingmecry/firstfirebase/MainActivity.java
@@ -0,0 +1,15 @@
+package com.yourmakingmecry.firstfirebase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //vsv
+    }
+}
