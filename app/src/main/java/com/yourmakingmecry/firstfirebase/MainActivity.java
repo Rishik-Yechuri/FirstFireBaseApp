2e8a746692e85d4d28eb8ecfd19e68b4e033d9e2
Rishik <epicrishik@gmail.com>
2020-06-22 13:53:20 -0500

    Proccess  Server Data

--- app/src/main/java/com/yourmakingmecry/firstfirebase/MainActivity.java
+++ app/src/main/java/com/yourmakingmecry/firstfirebase/MainActivity.java
@@ -1,12 +1,23 @@
 package com.yourmakingmecry.firstfirebase;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.functions.FirebaseFunctions;
+import com.google.firebase.functions.HttpsCallableResult;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.Console;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     TextView statusText;
@@ -16,8 +27,32 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         statusText = findViewById(R.id.statusText);
-        FirebaseFunctions.getInstance() // Optional region: .getInstance("europe-west1")
+        addMessage("dieselguy");
+        String filtered = String.valueOf(dataReturned);
+        Log.d("horses","Value of returned data:" + filtered);
+        /*FirebaseFunctions.getInstance() // Optional region: .getInstance("europe-west1")
                 .getHttpsCallable("createUser")
-                .call();
+                .call("hi");*/
+
+    }
+    private void addMessage(String text) {
+        // Create the arguments to the callable function.
+        Map<String, Object> data = new HashMap<>();
+        data.put("text", text);
+        FirebaseFunctions.getInstance()
+                .getHttpsCallable("createUser")
+                .call(data)
+                .continueWith(new Continuation<HttpsCallableResult, String>() {
+                    @Override
+                    public String then(@NonNull Task<HttpsCallableResult> task) throws Exception {
+                        // This continuation runs on either success or failure, but if the task
+                        // has failed then getResult() will throw an Exception which will be
+                        // propagated down.
+                        HashMap result = (HashMap) task.getResult().getData();
+                        JSONObject res = new JSONObject(result);
+                        //String message = res.getString("message");
+                        return null;
+                    }
+                });
     }
 }
